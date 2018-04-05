032cac44af2eee4eb1316f9bad0f6f3a5dbe7c21
Elad Keyshawn <elad.keyshawn@gmail.com>
2018-04-05 16:34:50 +0300

    init commit :fire:

---
+++ app/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+export default {
+
+    init: () => (state, actions) => {
+        console.log("initializing app state");
+    },
+
+
+};
---
+++ app/src/components/main.js
@@ -0,0 +1,8 @@
+import {h} from "hyperapp"
+
+export default (state, {init}) =>
+  <div  oncreate={init}>
+
+  </div>;
+
+
